be7a8a14ae91a415704bce10ebe3416d3529d80d
lichong <lc@molasync.com>
2019-06-20 18:01:53 +0800

    add config

--- app/joker.go
+++ app/joker.go
@@ -1,19 +1,23 @@
 package main
 
 import (
+	"fmt"
+	"os"
 	"github.com/zihuyishi/joker/web"
 	"github.com/zihuyishi/joker/web/utils"
 )
 
 func main() {
-	config := &utils.Config{
-		DBPass: "mysecretpassword",
-		DBUser: "postgres",
-		DBName: "joker",
-		DBAddr: "some-postgres:5432",
-		Addr: ":7001",
-
+	path, exists := os.LookupEnv("JOKER_CONFIG")
+	if !exists {
+		path = "./config/config.json"
 	}
+	config, err := utils.LoadConfigFromFile(path)
+	if err != nil {
+		fmt.Printf("read config error: %s", err.Error())
+		return
+	}
+
 	server := web.New(config)
 	server.Serve()
 }
--- web/utils/config.go
+++ web/utils/config.go
@@ -1,9 +1,25 @@
 package utils
 
+import (
+	"encoding/json"
+	"io/ioutil"
+)
+
 type Config struct {
 	DBAddr string
 	DBUser string
 	DBPass string
 	DBName string
 	Addr string
+}
+
+func LoadConfigFromFile(path string) (*Config, error) {
+	data, err := ioutil.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+
+	var config Config
+	err = json.Unmarshal(data, &config)
+	return &config, err
 }
